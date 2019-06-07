f0db931693b9d58f32037f2500ccd9857fd19129
Hexilee <hexileee@gmail.com>
2019-06-07 15:00:45 +0800

    backend development block, ready to ajust tables

--- handlers.py
+++ handlers.py
@@ -1,13 +1,20 @@
 from app import app
-from models import Order
+from models import *
 import json
 import session
+import time
+from flask import request
 from middleware import auth_guard
 
 @app.route('/api/order')
 @auth_guard
 def get_orders():
+    state = request.args.get('state', None)
+    end_time = time.localtime(request.args.get('end', time.time()))
+    page_size = request.args.get('size', 15)
     (username, typ) = session.get_user_data()
+    query = Buyer.query if typ == 1 else Seller.query
+    user = query.filter_by(username=username).first()
     return json.dumps(Order.query.all())
 
 @app.route('/api/mock/session', methods = ['POST'])
