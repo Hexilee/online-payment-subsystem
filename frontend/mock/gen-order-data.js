eab37a0958a0b065a6680d08f47faaa5267294f9
Hexilee <hexileee@gmail.com>
2019-06-11 03:04:50 +0800

    add mock/gen-order-data.js

---
+++ frontend/mock/gen-order-data.js
@@ -0,0 +1,26 @@
+const Mock = require('mockjs');
+const Random = Mock.Random;
+const randomInt = (upto) => Math.floor(Math.random() * Math.floor(upto));
+const gen = () => {
+    const items = [];
+    for (let key of new Array(1000).keys()) {
+        items.push({
+            OrderNo: key,
+            OrderState: randomInt(5),
+            GoodName: Random.csentence(),
+            GoodDescription: Random.cparagraph(),
+            BuyerId: 1,
+            SellerId: 1,
+            OrderTime: randomInt(1560109154),
+            PayTime: null,
+            DeliverTime: null,
+            SuccessTime: null,
+            CancelTime: null,
+            Amount: randomInt(10000)
+        });
+    }
+    return items
+};
+
+console.log(JSON.stringify(gen()));
+
