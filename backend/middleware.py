6898ffaa27a84775e412872bfe9131c2be0c087e
Hexilee <hexileee@gmail.com>
2019-06-12 15:44:16 +0800

    wrap authenticated_handler with handler

--- backend/middleware.py
+++ backend/middleware.py
@@ -1,6 +1,9 @@
 from flask import session
+from functools import wraps
+
 
 def auth_guard(handler):
+    @wraps(handler)
     def authenticated_handler(*args, **kwargs):
         if session.get('username') is None or session.get('userid') is None or session.get('type') is None:
             return b"", 401
