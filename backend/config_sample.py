bdb1366993d2f1ebc5afcc3fa12a34c4a726accc
Hexilee <hexileee@gmail.com>
2019-06-16 23:53:44 +0800

    update config_sample

--- backend/config_sample.py
+++ backend/config_sample.py
@@ -4,5 +4,5 @@ DB_NAME = "OnlinePayment"
 DB_USERNAME = ""
 DB_PASSWD = ""
 SESSION_SECRET_KEY = ""
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = True # SQLAlchemy 是否打 Log
 FLASK_ENV = 'dev' # 'prod' | 'dev' | 'test' 
